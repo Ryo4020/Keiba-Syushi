8485f6388eacdc4a0fa7e74858c9b01b9caa087a
Ryo <gryo.1440@gmail.com>
2022-12-22 22:41:10 +0900

    axiosをボタンで実装

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import axios from './providors/axios';
+
 function App() {
+  async function handleReadCode() {
+    const code = "10100022010110100200442287400909300994740630102030405000019012345678901234567890123456789012345"
+
+    const params = new URLSearchParams({
+      code: code
+    })
+
+    const response = await axios.get(`reader?${params}`);
+
+    console.log(response.data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="back">
+      <div className='App'>
+        <h1>
+          QRコードリーダー
+        </h1>
+        <button onClick={handleReadCode}>APIテスト</button>
+      </div>
     </div>
   );
 }
