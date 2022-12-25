258b1f29dab2b3004b60698700c687839d358116
Ryo <gryo.1440@gmail.com>
2022-12-26 00:04:54 +0900

    axiosのセットアップの修正

--- web/src/providors/axios.ts
+++ web/src/providors/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const instance = axios.create({
+export const axiosInstance = axios.create({
     baseURL: "http://localhost:8000/",
 });
 
-export default instance;
+export const isAxiosError = axios.isAxiosError
