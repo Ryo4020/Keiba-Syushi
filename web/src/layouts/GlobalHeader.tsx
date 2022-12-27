c0bf7034fd9f524e97f260eab2d9add6ee3debc3
Ryo <gryo.1440@gmail.com>
2022-12-27 15:10:18 +0900

    サービス名（現段階）の変更

--- web/src/layouts/GlobalHeader.tsx
+++ web/src/layouts/GlobalHeader.tsx
@@ -4,7 +4,7 @@ import Typography from '@mui/material/Typography';
 export default function GlobalHeader(): JSX.Element {
     return <AppBar position="static" color="primary">
         <Typography variant="h4" component="h1" align='center' sx={{margin: "8px 0"}}>
-            QRコードリーダー
+            馬券リーダー
         </Typography>
     </AppBar>
 }
