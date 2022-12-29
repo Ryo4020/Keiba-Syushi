4163199fc665036f926fc3114010356506f64d82
Ryo <gryo.1440@gmail.com>
2022-12-29 17:41:34 +0900

    marginの指定法変更

--- web/src/layouts/GlobalHeader.tsx
+++ web/src/layouts/GlobalHeader.tsx
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 
 export default function GlobalHeader(): JSX.Element {
     return <AppBar position="static" color="primary">
-        <Typography variant="h4" component="h1" align='center' sx={{margin: "8px 0"}}>
+        <Typography variant="h4" component="h1" align='center' sx={{my: "8px"}}>
             馬券リーダー
         </Typography>
     </AppBar>
